c7ce06d7242b3cd1550c05d7e8965d7dd4227da1
Chandra Wiratama Putera <141868663+Chandrax222@users.noreply.github.com>
2023-11-04 23:59:39 +0700

    OPerasi String dan concat
    OPerasi String dan concat

---
+++ Tugas27.js
@@ -0,0 +1,12 @@
+function Tugas27() {
+    var a = "NO"
+    var b = "rumah saya"
+    var c = 30
+    var d = "A"
+
+    const hasil = String(a)+" ".concat(b)+" "+String(c)+String(d)
+    const hasil2 = String(a)+" ".concat(b)+" "+String(c)+String(d)
+    console.log(hasil);
+    console.log(hasil2);
+}
+Tugas27();
